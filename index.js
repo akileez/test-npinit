6be57b6b38e097ba6104d004bbd67e28273374b0
akileez <git@keeda.net>
2021-04-18 09:21:41 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * test-npinit <https://github.com/akileez/test-npinit>
+ *
+ * Copyright (c) 2021 Keith Williams.
+ * Licensed under the ISC license.
+ */
+
+'use strict'
+
+module.exports = function () {
+  // do stuff
+}
