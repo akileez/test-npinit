094c91cd39b5322e98d2f53d702f157d07b2c556
akileez <git@keeda.net>
2021-05-01 08:43:47 -0400

    minor edit -- remove spaces, add comment

--- license.js
+++ license.js
@@ -40,6 +40,8 @@ const destination = 'tmp'
 const licensedir  = 'lib/license'
 
 async function routeLicense (fp) {
+  // read, expand and route are the functionality of function: processFile
+  // contained in tmpls.js
   // set up file object
   const file = File({path: fp})
   // read in file
@@ -75,132 +77,48 @@ const help = task.process('help', async (inp, out, opts) => {
 })
 
 // License file tasks
-task('afl3', async () => {
-  await routeLicense(`${licensedir}/afl-3.0.txt`)
-})
-task('agpl3', async () => {
-  await routeLicense(`${licensedir}/agpl-3.0.txt`)
-})
-task('apache', async () => {
-  await routeLicense(`${licensedir}/apache-2.0.txt`)
-})
-task('artistic', async () => {
-  await routeLicense(`${licensedir}/artistic-2.0.txt`)
-})
-task('bsd0', async () => {
-  await routeLicense(`${licensedir}/0bsd.txt`)
-})
-task('bsd2', async () => {
-  await routeLicense(`${licensedir}/bsd-2-clause.txt`)
-})
-task('bsd3', async () => {
-  await routeLicense(`${licensedir}/bsd-3-clause.txt`)
-})
-task('bsd3c', async () => {
-  await routeLicense(`${licensedir}/bsd-3-clause-clear.txt`)
-})
-task('bsd4', async () => {
-  await routeLicense(`${licensedir}/bsd-4-clause.txt`)
-})
-task('bsl', async () => {
-  await routeLicense(`${licensedir}/bsl-1.0.txt`)
-})
-task('cc4', async () => {
-  await routeLicense(`${licensedir}/cc-by-4.0.txt`)
-})
-task('cc4sa', async () => {
-  await routeLicense(`${licensedir}/cc-by-sa-4.0.txt`)
-})
-task('cc0', async () => {
-  await routeLicense(`${licensedir}/cc0-1.0.txt`)
-})
-task('cecill', async () => {
-  await routeLicense(`${licensedir}/cecill-2.1.txt`)
-})
-task('ecl', async () => {
-  await routeLicense(`${licensedir}/ecl-2.0.txt`)
-})
-task('epl', async () => {
-  await routeLicense(`${licensedir}/epl-1.0.txt`)
-})
-task('epl2', async () => {
-  await routeLicense(`${licensedir}/epl-2.0.txt`)
-})
-task('eupl', async () => {
-  await routeLicense(`${licensedir}/eupl-1.1.txt`)
-})
-task('eupl2', async () => {
-  await routeLicense(`${licensedir}/eupl-1.2.txt`)
-})
-task('gpl2', async () => {
-  await routeLicense(`${licensedir}/gpl-2.0.txt`)
-})
-task('gpl3', async () => {
-  await routeLicense(`${licensedir}/gpl-3.0.txt`)
-})
-task('isc', async () => {
-  await routeLicense(`${licensedir}/isc.txt`)
-})
-task('lgpl1', async () => {
-  await routeLicense(`${licensedir}/lgpl-1.3c.txt`)
-})
-task('lgpl2', async () => {
-  await routeLicense(`${licensedir}/lgpl-2.1.txt`)
-})
-task('lgpl3', async () => {
-  await routeLicense(`${licensedir}/lgpl-3.0.txt`)
-})
-task('mit0', async () => {
-  await routeLicense(`${licensedir}/mit-0.txt`)
-})
-task('mit', async () => {
-  await routeLicense(`${licensedir}/mit.txt`)
-})
-task('mpl', async () => {
-  await routeLicense(`${licensedir}/mpl-2.0.txt`)
-})
-task('mspl', async () => {
-  await routeLicense(`${licensedir}/ms-pl.txt`)
-})
-task('msrl', async () => {
-  await routeLicense(`${licensedir}/ms-rl.txt`)
-})
-task('mulanpsl', async () => {
-  await routeLicense(`${licensedir}/mulanpsl-2.0.txt`)
-})
-task('ncsa', async () => {
-  await routeLicense(`${licensedir}/ncsa.txt`)
-})
-task('no', async () => {
-  await routeLicense(`${licensedir}/no`)
-})
-task('odbl', async () => {
-  await routeLicense(`${licensedir}/odbl-1.0.txt`)
-})
-task('ofl', async () => {
-  await routeLicense(`${licensedir}/ofl-1.1.txt`)
-})
-task('osl', async () => {
-  await routeLicense(`${licensedir}/osl-3.0.txt`)
-})
-task('postgresql', async () => {
-  await routeLicense(`${licensedir}/postgresql.txt`)
-})
-task('un', async () => {
-  await routeLicense(`${licensedir}/unlicense.txt`)
-})
-task('upl', async () => {
-  await routeLicense(`${licensedir}/upl-1.0.txt`)
-})
-task('vim', async () => {
-  await routeLicense(`${licensedir}/vim.txt`)
-})
-task('wtfpl', async () => {
-  await routeLicense(`${licensedir}/wtfpl.txt`)
-})
-task('zlib', async () => {
-  await routeLicense(`${licensedir}/zlib.txt`)
-})
+task('afl3', async () => await routeLicense(`${licensedir}/afl-3.0.txt`))
+task('agpl3', async () => await routeLicense(`${licensedir}/agpl-3.0.txt`))
+task('apache', async () => await routeLicense(`${licensedir}/apache-2.0.txt`))
+task('artistic', async () => await routeLicense(`${licensedir}/artistic-2.0.txt`))
+task('bsd0', async () => await routeLicense(`${licensedir}/0bsd.txt`))
+task('bsd2', async () => await routeLicense(`${licensedir}/bsd-2-clause.txt`))
+task('bsd3', async () => await routeLicense(`${licensedir}/bsd-3-clause.txt`))
+task('bsd3c', async () => await routeLicense(`${licensedir}/bsd-3-clause-clear.txt`))
+task('bsd4', async () => await routeLicense(`${licensedir}/bsd-4-clause.txt`))
+task('bsl', async () => await routeLicense(`${licensedir}/bsl-1.0.txt`))
+task('cc4', async () => await routeLicense(`${licensedir}/cc-by-4.0.txt`))
+task('cc4sa', async () => await routeLicense(`${licensedir}/cc-by-sa-4.0.txt`))
+task('cc0', async () => await routeLicense(`${licensedir}/cc0-1.0.txt`))
+task('cecill', async () => await routeLicense(`${licensedir}/cecill-2.1.txt`))
+task('ecl', async () => await routeLicense(`${licensedir}/ecl-2.0.txt`))
+task('epl', async () => await routeLicense(`${licensedir}/epl-1.0.txt`))
+task('epl2', async () => await routeLicense(`${licensedir}/epl-2.0.txt`))
+task('eupl', async () => await routeLicense(`${licensedir}/eupl-1.1.txt`))
+task('eupl2', async () => await routeLicense(`${licensedir}/eupl-1.2.txt`))
+task('gpl2', async () => await routeLicense(`${licensedir}/gpl-2.0.txt`))
+task('gpl3', async () => await routeLicense(`${licensedir}/gpl-3.0.txt`))
+task('isc', async () => await routeLicense(`${licensedir}/isc.txt`))
+task('lgpl1', async () => await routeLicense(`${licensedir}/lgpl-1.3c.txt`))
+task('lgpl2', async () => await routeLicense(`${licensedir}/lgpl-2.1.txt`))
+task('lgpl3', async () => await routeLicense(`${licensedir}/lgpl-3.0.txt`))
+task('mit0', async () => await routeLicense(`${licensedir}/mit-0.txt`))
+task('mit', async () => await routeLicense(`${licensedir}/mit.txt`))
+task('mpl', async () => await routeLicense(`${licensedir}/mpl-2.0.txt`))
+task('mspl', async () => await routeLicense(`${licensedir}/ms-pl.txt`))
+task('msrl', async () => await routeLicense(`${licensedir}/ms-rl.txt`))
+task('mulanpsl', async () => await routeLicense(`${licensedir}/mulanpsl-2.0.txt`))
+task('ncsa', async () => await routeLicense(`${licensedir}/ncsa.txt`))
+task('no', async () => await routeLicense(`${licensedir}/no`))
+task('odbl', async () => await routeLicense(`${licensedir}/odbl-1.0.txt`))
+task('ofl', async () => await routeLicense(`${licensedir}/ofl-1.1.txt`))
+task('osl', async () => await routeLicense(`${licensedir}/osl-3.0.txt`))
+task('postgresql', async () => await routeLicense(`${licensedir}/postgresql.txt`))
+task('un', async () => await routeLicense(`${licensedir}/unlicense.txt`))
+task('upl', async () => await routeLicense(`${licensedir}/upl-1.0.txt`))
+task('vim', async () => await routeLicense(`${licensedir}/vim.txt`))
+task('wtfpl', async () => await routeLicense(`${licensedir}/wtfpl.txt`))
+task('zlib', async () => await routeLicense(`${licensedir}/zlib.txt`))
 
 task('_clean', async () => {
   // nice little clean-up app right here
